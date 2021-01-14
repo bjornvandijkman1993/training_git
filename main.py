01ca91f2acefc102d58709f3bc7de9e2e1f5b736
bvandijkman <bvandijkman@deloitte.nl>
2021-01-14 11:14:49 +0100

    Add a line of code.

--- main.py
+++ main.py
@@ -4,4 +4,6 @@ import pandas as pd
 data = pd.read_csv("data/titanic.csv", sep = ';')
 
 st.title("The titanic dataset")
-st.dataframe(data)
+
+rows_to_show = st.slider("How many rows of data do you want to show?", 0, 100, 10)
+st.dataframe(data.head(rows_to_show))
